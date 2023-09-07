881af7007cd36f7d4ec61b52351c0e43b605e9d0
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-07 17:12:35 +0300

    implement task

---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  const { slack_name, track } = req.query;
+
+  if (!slack_name) {
+    return res.status(400).json({
+      message: "Please provide a slack name in the query params as slack_name",
+      status_code: 400,
+    });
+  }
+
+  if (!track) {
+    return res.status(400).json({
+      message: "Please provide a track in the query params as track",
+      status_code: 400,
+    });
+  }
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  res.status(200).json({
+    slack_name,
+    current_day: days[new Date().getDay()],
+    utc_time: new Date().toUTCString(),
+    track,
+    github_file_url:
+      "https://github.com/akuya-ekorot/hngx-backend-task-one/blob/main/",
+    github_repo_url: "https://github.com/akuya-ekorot/hngx-backend-task-one",
+    status_code: 200,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
