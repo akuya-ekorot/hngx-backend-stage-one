860c4b6f0d4044efdade6c284cf8b071b738847f
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-07 17:27:58 +0300

    change endpoint to api

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   const { slack_name, track } = req.query;
 
   if (!slack_name) {
